96c3b66af80910113a0bb0990af50a233c1cb228
Artem Fomin <a.v.fomin@stratman.pro>
2023-10-14 23:52:36 +0300

    2023-10-14

--- app.py
+++ app.py
@@ -2,6 +2,25 @@ import flask
 from flask import render_template
 import pickle
 import sklearn
-from sklearn.linear_model import LinearRegression
+from sklearn.multioutput import MultiOutputRegressor
 
-print('Hello EBW Predict!')
+app = flask.Flask(__name__, template_folder='src/templates', static_folder='src')
+
+
+@app.route('/', methods = ['POST', 'GET'])
+
+@app.route('/index', methods = ['POST', 'GET'])
+def main():
+    if flask.request.method == 'GET':
+        return render_template('index.html')
+    if flask.request.method == 'POST':
+        with open('src/models/mor_model.pkl', 'rb') as f:
+            loaded_model = pickle.load(f)
+        
+        input_data = flask.request.form[['iw', 'if', 'vw', 'fp']]
+        y_pred = loaded_model.predict([[input_data]])
+        
+        return render_template('index.html', result = y_pred)
+    
+if __name__ == '__main__':
+    app.run()
